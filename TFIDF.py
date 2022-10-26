5f61745a7dacbb1569ccc45ef2f4dda3dcaafd68
cynthiachennn <94323798+cynthiachennn@users.noreply.github.com>
2022-10-26 00:28:44 -0400

    tfidf is done maybe?

--- TFIDF.py
+++ TFIDF.py
@@ -1,4 +1,3 @@
-#DUPLICATE TEST VERSION
 import math
 import re
 
@@ -51,14 +50,14 @@ def gettf(docname):
             freqs[word] += 1
         else:
             freqs[word] = 1
-
-    #print("tf freqs: " + str(freqs) + "\n") FREQUENCY/COUNT SEEMS TO BE CORRECT
+    
     for (word, count) in freqs.items():
         freqs[word] = count/tot
+
     return freqs
 
-def getidf(tf, doclist):
-    #compute inverse document frquency of each word for each jdocument
+def getidf(tf, doclist):    
+    #compute inverse document frquency of each word for eac jdocument
     doccount = len(tf)
     wordlist = {}
     for doc in doclist: #for each document
@@ -67,13 +66,11 @@ def getidf(tf, doclist):
                 wordlist[word] += 1 #add to "# of documents term t in in"
             else:
                 wordlist[word] = 1
-    #print("total number of documents: " + str(doccount) + "\n" + "# of documents term is in: " + str(wordlist) )
+    
     idf = {}
     for word in wordlist:
-        idf[word] = math.log(doccount/wordlist[word]) + 1 #ln((total nmber of documents)/(# of documents term t is in)) + 1
-    #print("idf: " + str(idf) + "\n")
+        idf[word] = math.log(doccount/wordlist[word] + 1) #ln((total nmber of documents)/(# of documents term t is in)) + 1
     return idf
-    
 
 
 def gettfidf(doc, tf, idf):
@@ -81,43 +78,41 @@ def gettfidf(doc, tf, idf):
     tfidf = {} #word:tfidf score dic
     for word in tf:
         tfidf[word] = round(tf.get(word) * idf.get(word), 2) #calculate tfidf for each word.
+        print(word + ": " + str(tfidf[word]) + " " + str(tf.get(word)) + " " + str(idf.get(word)))
+    print(tfidf)
+    
+    sdic = dict(sorted(tfidf.items(), key=lambda item: (item[1], item[0]))) #get top 5
+    t5 = list(sdic.items())[:4]
 
-    t5 = []
-    for i in range(5):
-        max = ("", 0)
-        for k, v in tfidf.items(): #iterate thru dict
-            if v > max[1]: #if this is max
-                max = (k, v) #save it
-            if v == max[1]: #tiebreakers
-                if k < max[0]:
-                    max = (k, v)
-        t5.append(max) #save actual max
-        del tfidf[max[0]] #delete it from list 
-    
     with open(f'tfdif_{doc}', 'w') as f:
-        f.write(str(t5))
-    
-    #print to tfidf_[document name].txttop 5 most important words in each prepreocessed document according to their tf-idf scores
+        f.write("[")
+        for x, y in t5:
+            f.write(str(list))
+            f.write("(" + x + ", " + str(y) + ")")
+        f.write("]")
+    #print to tfidf_[document name].txttop 4 most important words in each prepreocessed document according to their tf-idf scores
         #in case of ties, pick words in alcfabetical order
         #print result as list: (word, TF-IDF score)
 
 def main():
     doclist = []
+    preproclist = []
     stopword() #read stopwords from doc and make into list
-    with open("idf-idf.txt", "r") as f:
+    with open("tf-idf.txt", "r") as f:
         input = f.read()
         doclist = input.split('\n')
-            
-        #compute tfidf
+        for docname in doclist:
+            preproclist.append(preprocess(docname)) #preprocess that doc, add new docname to list
+              
+        #compute tdidf
         #print top tfidf
     tf = {} #empty list of tf dictionaries for each doc. SAME ORDER AS DOCLIST
-    for docname in doclist: #for each document...
-        tf[docname] = gettf(f'preproc_{docname}') #create a dictionary of word:tf and add dictionaries to list.
-    idf = getidf(tf, doclist) #create word:idf dictionary per word for all docs
+    for docname in preproclist: #for each document...
+        tf[docname] = tf(docname) #create a dictionary of word:tf and add dictionaries to list.
+        idf = idf(tf) #create word:idf dictionary per word for all docs
     #actually:
     for doc in doclist:
         gettfidf(doc, tf[doc], idf) #create word:tfidf and print top 5
-
 
 def test():
     doclist = []
@@ -131,9 +126,11 @@ def test():
     tf = {} #empty list of tf dictionaries for each doc. SAME ORDER AS DOCLIST
     for docname in doclist: #for each document...
         tf[docname] = gettf(f'preproc_{docname}') #create a dictionary of word:tf and add dictionaries to list.
+        print(tf[docname])
     idf = getidf(tf, doclist) #create word:idf dictionary per word for all docs
     #actually:
     for doc in doclist:
         gettfidf(doc, tf[doc], idf) #create word:tfidf and print top 5
 
-main()
+
+test()
